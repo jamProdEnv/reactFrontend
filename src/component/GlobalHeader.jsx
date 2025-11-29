cbb8d7689d8bee9b6b9b8bb92f4c999e6a1ba691
Elusive Sky <74522772+JoshuaAubryM@users.noreply.github.com>
2025-11-29 11:31:06 -0800

    GlobalHeader.jsx and module.css

--- src/component/GlobalHeader.jsx
+++ src/component/GlobalHeader.jsx
@@ -4,30 +4,32 @@ import { GlobalStatus } from "./GlobalStatus";
 export function GlobalHeader() {
   return (
     <>
-      <header className={classes.globalHeader}>
-        <div className={classes.headerDiv1}>
-          <h1 className={classes.globalH1}>Portfolio</h1>
-          <div className={classes.globalStatus}>
+      <header className={classes.header}>
+        <div className={classes.div1}>
+          <h1>Portfolio</h1>
+          <div className={classes.status}>
             <GlobalStatus />
           </div>
         </div>
 
-        <nav className={classes.globalNav}>
-          <ul className={classes.globalHeaderUL}>
-            <li>
-              <Link to={"/"}>Main</Link>
-            </li>
-            <li>
-              <Link to={"/resume"}>Profile</Link>
-            </li>
-            <li>
-              <Link to={"/chat"}>Chat</Link>
-            </li>
-            <li>
-              <Link to={"/blog"}>Blog</Link>
-            </li>
-          </ul>
-        </nav>
+        <div className={classes.div2}>
+          <nav className={classes.nav}>
+            <ul className={classes.links}>
+              <li>
+                <Link to={"/"}>Main</Link>
+              </li>
+              <li>
+                <Link to={"/resume"}>Profile</Link>
+              </li>
+              <li>
+                <Link to={"/chat"}>Chat</Link>
+              </li>
+              <li>
+                <Link to={"/blog"}>Blog</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </>
   );
