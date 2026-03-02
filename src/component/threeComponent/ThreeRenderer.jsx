4a51a3faff1bd9bb4e10eb0b578c721b2e8d41ff
jamProdEnv <jam.prodEnv@yahoo.com>
2026-03-01 21:00:16 -0800

    renderer fix in ThreeRenderer()

--- src/component/threeComponent/ThreeRenderer.jsx
+++ src/component/threeComponent/ThreeRenderer.jsx
@@ -5,7 +5,8 @@ let renderer;
 export function ThreeRenderer(){
     
         renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setPixelRatio(window.devicePixelRatio);
+        // renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
         renderer.shadowMap.enabled = true;
     
     return renderer;
