e45b12821ccb0c2e2a8ee951542fbe2f659cb22a
jamProdEnv <jam.prodEnv@yahoo.com>
2026-03-01 20:27:40 -0800

    git reset --hard c53d12c after: ladningPage and LandingPage css

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -4,6 +4,7 @@ import { Sprite } from "./threeComponent/Sprite";
 import { DominoPhysics } from "./threeComponent/DominoPhysics";
 import classes from "../CSS/LandingPage.module.css";
 
+
 const items = [
   { type: "img", src: "/models1.jpg" },
   { type: "img", src: "/models2.jpg" },
@@ -11,23 +12,26 @@ const items = [
   { type: "img", src: "/models4.jpg" },
   { type: "img", src: "/models5.png" },
   { type: "img", src: "/models6.jpg" },
-  { type: "img", src: "/models7.png" },
+  {type: "img", src: "/models7.png"},
+ 
   { type: "component", label: "Sprite Raindrops", component: Sprite },
   { type: "component", label: "Cube Geometry", component: CubeGeometry },
   { type: "component", label: "Domino Physics", component: DominoPhysics },
 ];
 
 export function LandingPage() {
+ 
+  const [expandedIndex, setExpandedIndex] = useState(null);
   const [activeIndex, setActiveIndex] = useState(null);
   const ActiveItem = activeIndex !== null ? items[activeIndex] : null;
+ 
+  const expandedItem =
+    expandedIndex !== null ? items[expandedIndex] : null;
 
   return (
     <div className={classes.container}>
-      {/* ============================= */}
-      {/* Full-screen card/component */}
-      {/* ============================= */}
-       {/* FULLSCREEN MODAL */}
-      {/* {ActiveItem && (
+     
+      {expandedItem && (
         <div
           className={classes.modalOverlay}
         
@@ -35,83 +39,75 @@ export function LandingPage() {
           
           <div
             className={classes.modalContent}
-          
-          
+           
+            onClick={(e) => e.stopPropagation()}
           >
-            <button className={classes.modalClose}   onClick={() => setActiveIndex(null)}>close</button>
-            {ActiveItem.type === "img" && (
-              <img src={ActiveItem.src} alt=""/>
+            <button className={classes.modalClose}   onClick={() => setExpandedIndex(null)}>close</button>
+            {expandedItem.type === "img" && (
+              <img src={expandedItem.src} alt="" className={classes.fullImage}/>
             )}
 
-            {ActiveItem.type === "component" && (
-              <div className={classes.threeWrapper}>
-                <ActiveItem.component />
-                 </div>
+            {expandedItem.type === "component" && (
+              // <div key={expandedIndex} className={classes.threeWrapper}>
+                <expandedItem.component />
+                //  {/* </div> */}
               
             )}
           </div>
         </div>
-      )} */}
-      {ActiveItem && (
-        <div className={classes.fullViewOverlay}>
-          <button
-            className={classes.modalClose}
-            onClick={() => setActiveIndex(null)}
-          >
-            Close
-          </button>
-
-          {ActiveItem.type === "img" && (
-            <img src={ActiveItem.src} alt="" className={classes.fullImage} />
-          )}
-
-          {ActiveItem.type === "component" && (
-            // <div className={classes.threeWrapper}>
-              <ActiveItem.component />
-            // </div>
-          )}
-        </div>
       )}
 
-      {/* ============================= */}
-      {/* Regular Banner + Carousel */}
-      {/* ============================= */}
       <div className={classes.banner}>
-        {!ActiveItem && (
-          <div className={classes.slider} style={{ "--quantity": items.length }}>
-            {items.map((item, index) => (
+  
+        <div
+          className={classes.slider}
+          style={{ "--quantity": items.length }}
+        >
+          {items.map((item, index) => {
+            const Component = item.component;
+
+            return (
               <div
                 key={index}
                 className={classes.item}
                 style={{ "--position": index + 1 }}
-                onClick={() => setActiveIndex(index)}
+                onClick={() => setExpandedIndex(index)}
               >
-                {item.type === "img" && <img src={item.src} alt="" />}
+                {/* {item.type === "img" && (
+                  <img src={item.src} alt="" />
+                )}
+
                 {item.type === "component" && (
                   <div className={classes.image}>
-                    (Click) <br />
+                    (Click) <br/>
                     {item.label}
                   </div>
-                )}
+                )} */}
+                 {item.type === "img" && <img src={item.src} alt="" />}
+                                {item.type === "component" && (
+                                  <div className={classes.image}>
+                                    (Click) <br />
+                                    {item.label}
+                                  </div>
+                                )}
               </div>
-            ))}
-          </div>
-        )}
-      </div>
-
-      {/* ============================= */}
-      {/* Content, author, and model remain */}
-      {/* ============================= */}
-      <div className={classes.content}>
-        <h1 data-content="WSJR PORTFOLIO">WSJR PORTFOLIO</h1>
-
-        <div className={classes.author}>
-          <h2>Joshua Middleton</h2>
-          <p>Web Development</p>
-          <p>Welcome To My Portfolio</p>
+            );
+          })}
         </div>
 
-        <div className={classes.model}></div>
+        <div className={classes.content}>
+          <h1 data-content="WSJR PORTFOLIO">
+            WSJR PORTFOLIO
+          </h1>
+
+          <div className={classes.author}>
+            <h2>Joshua Middleton</h2>
+            <p>Web Development</p>
+            <p>Welcome To My Portfolio</p>
+          </div>
+
+          <div className={classes.model}></div>
+        </div>
       </div>
     </div>
   );
